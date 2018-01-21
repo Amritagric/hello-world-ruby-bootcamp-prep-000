4fe4e7087aaa8e96ff36de0a0a1ef4dbac2d4c26
A O <amrita.olivieri@gmail.com>
2018-01-21 23:12:55 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,13 +1,3 @@
 hello world.RUBY_VERSION
 touch hello_world.RUBY_VERSION
 puts "Hello World"
-ruby hello_world.ruby
-learn submit_tag
-puts "Hello World"
-ruby hello_world.ruby
-
-
-
-
-touch hello_world.ruby
-atom hello_world.ruby
